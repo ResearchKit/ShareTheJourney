d325ea42e5f9cc8288b069868cec45751c3dd201
Matt Morton <mmorton@torpedogroup.com>
2015-06-12 10:11:22 +0100

    Tidying up the app delegate
    Inconsistent whitespace and use of scope brackets, Tidying up to be
    more consistent. Also changed the UIKit import to use a semantic import
    of the UIKit module.

--- BreastCancer/Startup/APHAppDelegate.h
+++ BreastCancer/Startup/APHAppDelegate.h
@@ -31,7 +31,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
  
-#import <UIKit/UIKit.h>
+@import UIKit;
 @import APCAppCore;
 
 @interface APHAppDelegate : APCAppDelegate
